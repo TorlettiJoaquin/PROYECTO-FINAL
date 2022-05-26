04b3bb2f77729c34267e0319165aec8eba0bac3f
TorlettiJoaquin <Axeltorletti@gmail.com>
2022-05-26 20:03:17 -0300

    Se crea BACKEND y se agrega spring

---
+++ Portfolio_TorlettiAxel/BackEnd/src/main/java/com/portfolio/taj/TajApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.taj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TajApplication.class, args);
+	}
+
+}
---
+++ Portfolio_TorlettiAxel/BackEnd/src/test/java/com/portfolio/taj/TajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.taj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
