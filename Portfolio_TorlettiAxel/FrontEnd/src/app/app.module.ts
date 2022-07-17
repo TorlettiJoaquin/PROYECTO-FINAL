3e08784213d7eaf6c548946893cbf735f14ff391
TorlettiJoaquin <Axeltorletti@gmail.com>
2022-07-17 18:55:14 -0300

    SE RE APLICA TODO EL FRONT COMPONENTS

--- Portfolio_TorlettiAxel/FrontEnd/src/app/app.module.ts
+++ Portfolio_TorlettiAxel/FrontEnd/src/app/app.module.ts
@@ -4,15 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HssComponent } from './components/hss/hss.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AcercaDeComponent,
+    BannerComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    FooterComponent,
+    HssComponent,
+    LogoApComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
